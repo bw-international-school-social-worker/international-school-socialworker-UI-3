11af6764b7ebd8e07bdedfbe8b4d711a8295ee72
74ultra <r.barnhill@protonmail.com>
2019-06-26 14:57:37 -0700

    Added event listeners for links and buttons

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,4 +1,8 @@
 
+
+// Purpose: When the 'open / close' button is clicked the .article element expands to show the associated picture and text
+
+
 // Article class defined here
 class Article {
     constructor(domElement){
@@ -18,15 +22,6 @@ class Article {
     }
 
 }
-
-
-
-
-
-
-
-
-
 
 
 
